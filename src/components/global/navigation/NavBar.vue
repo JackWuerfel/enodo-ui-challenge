7a523dfab4d9d9f3b8c5b6169c471d2fe3a05eb1
Jack Wuerfel <jwuerfel23@gmail.com>
2020-03-05 21:58:20 -0500

    additional object fix

--- src/components/global/navigation/NavBar.vue
+++ src/components/global/navigation/NavBar.vue
@@ -118,7 +118,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class NavBar extends Vue {
     @Prop()
-    private avatar!: bool;
+    private avatar!: object[];
 
     @Prop()
     private remainingProperties!: string;
